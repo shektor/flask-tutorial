313f81eac03d125787de5930fd4ca33caf44422a
shektor <s.hektor@gmail.com>
2019-09-05 23:38:35 +0100

    Register database with application
    A function is required to take the flask app and register the database. 
    This is due to the app being created with a factory function.
    
    **app.teardown_appcontext()** will call the passed function arguement 
    when cleaning up after a response.
    
    **app.cli.add_command()** adds a new command that can be called with 
    flask. In this case it will add the function previously defined in the 
    db file.

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,6 +5,11 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
 def init_db():
     db = get_db()
 
