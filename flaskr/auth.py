f15af0dfec0619077018f9917bbedcfcd5200345
shektor <s.hektor@gmail.com>
2019-09-06 23:25:32 +0100

    Create register view
    When the user visits the `/auth/register` URL, the register view should
    return a HTML form where they can submit a username and password.
    
    The route decorator associates the url with the view function.
    
    If the request object has the method set to 'GET', the register template
    will be rendered.
    
    If the method is set to 'POST', the username and password will be taken
    from the form dict which is a special type mapping submitting form keys
    and values. These will be inserted into the user table, with the
    password hashed. The changes are committed and the user is redirected to
    the login page.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,3 +1,29 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from werkzeug.security import generate_password_hash
+
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db
+
+        db.execute(
+            'INSERT INTO user (username, password) VALUES (?, ?)',
+            (username, generate_password_hash(password))
+        )
+        db.commit()
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html')
