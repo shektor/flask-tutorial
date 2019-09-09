bc0604fa94b7f744005db6d00a44d94e8dd706cd
shektor <s.hektor@gmail.com>
2019-09-09 22:54:32 +0100

    Add register validation to server
    While the register template has a required attribute for the username 
    and password, the user may be using a browser that does not support it, 
    or may be making a request using something other than a browser. This 
    means username and password validation should also be added to the 
    server.
    
    Use the flash module, which stores messages that can be retrieved when 
    rendering the template.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,5 +1,6 @@
 from flask import (
     Blueprint,
+    flash,
     redirect,
     render_template,
     request,
@@ -18,12 +19,21 @@ def register():
         username = request.form['username']
         password = request.form['password']
         db = get_db()
+        error = None
 
-        db.execute(
-            'INSERT INTO user (username, password) VALUES (?, ?)',
-            (username, generate_password_hash(password))
-        )
-        db.commit()
-        return redirect(url_for('auth.login'))
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
 
     return render_template('auth/register.html')
