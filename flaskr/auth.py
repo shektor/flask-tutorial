3120fa11324f482ce371f8a88b8ec843c42ce387
shektor <s.hektor@gmail.com>
2019-09-10 22:32:52 +0100

    Add load logged in user function
    At the beginning of each request, if a user is logged in their 
    information should be loaded and made available to other views.
    
    Use the `before_app_request` blueprint function to execute a 
    `load_logged_is_user` function that will execute before each request, 
    even if outside of the blueprint.
    
    The function will use attempt to use the `user_id` stored in the session 
    to populate the `g.user` value if the user has already logged in.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint,
     flash,
+    g,
     redirect,
     render_template,
     request,
@@ -12,6 +13,18 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
 
 @bp.route('/register', methods=('GET', 'POST'))
