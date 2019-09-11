bfc8de9128ed25f010b3084823834b2fcfbaf3f4
shektor <s.hektor@gmail.com>
2019-09-11 23:19:01 +0100

    Create blog index view
    The index view for the blog should show all of the posts, with the most
    recent first. The data query includes a join between the posts and user
    table so that the author can be identified.
    
    Post data is assigned to a variable and passed to the index template via
    a second arguement.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -38,3 +38,14 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
