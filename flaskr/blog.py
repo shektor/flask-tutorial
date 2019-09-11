1904a0c0e3d3c7adfd0da0a0d15ea615d7ab84cd
shektor <s.hektor@gmail.com>
2019-09-11 22:20:54 +0100

    Create blog create view and template
    The create view for the blog should work in the same way as the auth
    register view. If there is a `GET` request to the route, it should
    render a template with a form to complete, that includes a title and
    body.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,3 +1,11 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    render_template
+)
 
 bp = Blueprint('blog', __name__)
+
+
+@bp.route('/create')
+def create():
+    return render_template('blog/create.html')
