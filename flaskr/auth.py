0a589d8869fcc724fdce6438a5072e4ebf19f97e
shektor <s.hektor@gmail.com>
2019-09-11 23:11:39 +0100

    Fix login_required bug
    Functools method is wraps.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -91,7 +91,7 @@ def logout():
 
 
 def login_required(view):
-    @functools.wrap(view)
+    @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
             return redirect(url_for('auth.login'))
