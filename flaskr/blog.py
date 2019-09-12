c7b49bde3f5aa62e12167ceeb7d7dbbf7e6a343d
shektor <s.hektor@gmail.com>
2019-09-12 12:38:20 +0100

    Create get_post helper function
    Both the blog update and delete views need to fetch a post by id and
    check if the author matches the logged in user. To avoid duplication
    create a helper function and call it from each view.
    
    The helper function should retrieve the post details from the database
    using the id, or return an error based on whether the post exists, or if
    the user is not the author.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -55,18 +55,7 @@ def index():
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, "Post id {0} does not exist.".format(id))
-
-    if post['author_id'] != g.user['id']:
-        abort(403)
+    post = get_post(id)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -94,8 +83,26 @@ def update(id):
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
+    get_post(id)
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
 
     return redirect(url_for('blog.index'))
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} does not exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
